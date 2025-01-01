89a377a33294b39b9eb93e8a21fa421b4c0f1e64
SidBotz <sidop148@gmail.com>
2025-01-01 11:32:54 +0530

    Create index.js

---
+++ pages/play/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>Welcome to the Stream Player</h1>
+      <p>Use the format <strong>/play/[video_id]</strong> to play a video.</p>
+    </div>
+  );
+}
